e6cb668565d7dc3a42acc825424459d0a6fd69d0
WeiChien Hsu <weichien711@gmail.com>
2018-09-29 22:12:54 -0500

    :construction: Finished Single Resposibility

--- SingleResponsibilityPrinciple/main.cpp
+++ SingleResponsibilityPrinciple/main.cpp
@@ -11,12 +11,53 @@ struct Journal {
     string title;
     vector<string> entries;
 
+    Journal(const string &title) : title(title) {}
 
+    void add_entry(const string &entry)
+    {
+        static int count = 1;
+        entries.push_back(lexical_cast<string>(count++) + ":" + entry);
 
+    }
+
+    /* Wrong Way to save: Adding a persistent code into file */
+    /* If we would like to change the way we save the strings */
+    /* we need to go through all methods to change every implementation */
+    void save(const string& filename)
+    {
+        ofstream ofs(filename);
+        for(auto &e : entries) {
+            ofs << e << endl;
+        }
+    }
+};
+
+/* Separation of concern: to implement persistence */
+struct PersistenceManager
+{
+    /* We just need to refactor persistence and could change */
+    /* to way we want to save the contents to database       */
+    static void save(const Journal& j, const string& filename)
+    {
+        ofstream ofs(filename);
+        for(auto &e : j.entries) {
+            ofs << e << endl;
+        }
+    }
 };
 
 
 int main() {
+
+    Journal journal{"Dear Diary"};
+    journal.add_entry("I ate a bug.");
+    journal.add_entry("I cried today");
+
+    journal.save("diary.txt");
+
+    PersistenceManager pm;
+    pm.save(journal, "diary.txt")
+
     getcahr();
     return 0;
 }
