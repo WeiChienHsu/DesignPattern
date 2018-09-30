cdac73cb79b498d7cc562c2d665eb753cae4735a
WeiChien Hsu <weichien711@gmail.com>
2018-09-29 21:57:44 -0500

    :construciton: Add SingleResponsibilityPrinciple

---
+++ SingleResponsibilityPrinciple/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by weichien on 29/09/2018.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+
+struct Journal {
+    string title;
+    vector<string> entries;
+
+
+
+};
+
+
+int main() {
+    getcahr();
+    return 0;
+}
+
